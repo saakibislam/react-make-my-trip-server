e343f4ef1c3dd5c27a6dd7d31d52ae2b82d5eecc
saakibislam <sakib.cseacademic@gmail.com>
2021-11-01 12:00:37 +0600

    start-dev & process port added for heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
-const port = 5000;
+const port = process.env.PORT || 5000;
 require('dotenv').config()
 
 // Middleware
